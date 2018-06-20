411ff25c6192f72cd501c030cb9d459b515565a7
Rishi <rishiraja@gmail.com>
2018-06-19 19:10:26 -0700

    Bump to v1.0.1

--- src/main/java/blobsaver/Main.java
+++ src/main/java/blobsaver/Main.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    static final String appVersion = "v1.0";
+    static final String appVersion = "v1.0.1";
     static Stage primaryStage;
 
     public static void main(String[] args) {
